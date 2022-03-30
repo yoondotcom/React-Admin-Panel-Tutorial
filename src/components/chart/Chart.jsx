fe24087bb1dc3ec577828b99ea28d5a715687dc6
kimjiyun <didany15@dongguk.ac.kr>
2022-03-30 21:41:15 +0900

    테이블 작성

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -14,7 +14,7 @@ const Chart = () => {
     return (
         <div className="chart">
             <div className="title">Last 6 Months (Revenue)</div>
-            <ResponsiveContainer width="100%" aspect={3/1}>
+            <ResponsiveContainer width="100%" aspect={2/1}>
                 <AreaChart width={730} height={250} data={data}
                   margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                   <defs>
